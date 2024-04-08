ef5cb68db22b0327dd617d064d2fd58794c8eeb8
LupusVII <julien@webexmachina.fr>
2024-04-08 14:48:44 +0200

    Adjust vars

--- src/Sherlock/Wrapper.php
+++ src/Sherlock/Wrapper.php
@@ -32,16 +32,41 @@ class Wrapper
         $this->api = 'DEV' === $this->mode ? $this->api_dev : $this->api_prod;
         $this->data = $data;
         $this->message = array();
+
+        $this->data['keyVersion'] = $this->secret_key;
+        $this->data['merchantId'] = $this->merchant_id;
+
+        // Setup mandatory vars
+        if (!array_key_exists('interfaceVersion', $this->data)) {
+        	$this->data['interfaceVersion'] = 'HP_3.2';
+        }
+
+        if (!array_key_exists('currencyCode', $this->data)) {
+        	$this->data['currencyCode'] = '978';
+        }
+
+        if (!array_key_exists('orderChannel', $this->data)) {
+        	$this->data['orderChannel'] = 'INTERNET';
+        }   
+    }
+
+    public function __get($key)
+    {
+    	return $this->data[$key];
+    }
+
+    public function __set($key, $value)
+    {
+    	$this->data[$key] = $value;
     }
 
     // paymentInit endpoint
-    // Mandatory: Data, InterfaceVersion, Seal
-    // Optional: Encode, SealAlgorithm
+    // Mandatory: amount, currencyCode, interfaceVersion, keyVersion, normalReturnUrl, orderChannel Seal
     // Data Example: amount=5500|currencyCode=978|merchantId=011223744550001|normalReturnUrl=http://www.normalreturnurl.com|transactionReference=534654|keyVersion=1
     // Doc: https://sherlocks-documentation.secure.lcl.fr/fr/dictionnaire-des-donnees/paypage/paymentwebinit.html
     public function paymentInit()
     {
-    	$url = $this->api . 'paymentInit';
+    	$this->api_method('paymentInit');
 
     	
     }
@@ -54,5 +79,30 @@ class Wrapper
     protected function getSeal()
     {
     	return hash_hmac($this->data['hashAlgorithm1'] ?: 'sha256', $this->data, $this->secret_key);
+    }
+
+
+    public function api_method($endpoint, $args, $type = 'POST')
+    {
+    	$url = $this->api . $endpoint;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_COOKIESESSION, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $type);
+
+        if ('POST' === $type) {
+        	$query = http_build_query($args);
+        	curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
+        }
+
+        $result = curl_exec($ch);
+
+        $this->message = array();
+        $this->add_response($result);
+        curl_close($ch);
     }
 }
